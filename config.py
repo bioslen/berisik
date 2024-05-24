0129fba2d2ce0dbaa96e31a0d3aa60a29ad2cb4a
bioslen <157968855+bioslen@users.noreply.github.com>
2024-05-24 14:42:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -85,13 +85,13 @@ PING_IMG_URL = getenv(
 PLAYLIST_IMG_URL = "https://telegra.ph/file/d910922c056b74d9e3ee1.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/33f9cd183446c43e8bb60.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/b1314ac82cb9f7a941a9a.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/26afe631efa9fac6e0c4b.jpg"
 STREAM_IMG_URL = "https://telegra.ph/file/687ee982f4584af1e9f6d.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/b94f844446e518b2855f7.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/ce9e180952b33dbaa57bb.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/ce9e180952b33dbaa57bb.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/ce9e180952b33dbaa57bb.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/b1314ac82cb9f7a941a9a.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/6b3bb3420df040929ad34.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6b3bb3420df040929ad34.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6b3bb3420df040929ad34.jpg"
 
 
 def time_to_seconds(time):
