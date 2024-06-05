b7970e4f2b52dd7df3158167b6e6ade83459b0be
bioslen <157968855+bioslen@users.noreply.github.com>
2024-06-05 11:10:30 +0700

    autoleave.py

--- SlayerX/plugins/misc/autoleave.py
+++ SlayerX/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002194229196
-                                and i.chat.id != -1002194229196
+                                and i.chat.id != -1002243529302
+                                and i.chat.id != -1002243529302
                             ):
                                 if left == 20:
                                     continue
